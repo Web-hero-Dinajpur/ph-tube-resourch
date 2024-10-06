6ee8e98467594b8c657918c177dddc02a7c8cb57
Sreedhar-roy <sreedharroy0@gmail.com>
2024-10-06 13:36:21 +0600

    video section commit

--- video/video.js
+++ video/video.js
@@ -5,10 +5,42 @@ const loaddarafunction = ()=>{
     .catch(error => console.log(error));
 }
 
+const loadvideo = () =>{
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => loaddisplayvideo(data.videos))
+    .catch(error => console.log(error));
+}
+const loaddisplayvideo = (videos) =>{
+    const videocontainer = document.getElementById('cardcontainer')
+    videos.forEach(video => {
+        console.log(video);
+    const card = document.createElement('div')
+    card.classList ='card card-compact bg-base-100 w-96 shadow-xl'
+    card.innerHTML=`
+      <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `;
+    videocontainer.append(card)
+    })
+    
+}
+
 const loaddisplay = (datas)=>{
     const categoricontainer = document.getElementById('categories');
+
     datas.forEach(items => {
-        console.log(items);
+        // console.log(items);
     const button = document.createElement('button');
     button.classList ='btn';
     button.innerText = items.category;
@@ -17,3 +49,4 @@ const loaddisplay = (datas)=>{
     });
 }
 loaddarafunction();
+loadvideo();
